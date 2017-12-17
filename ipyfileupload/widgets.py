95ca8c17c6ee0fb113ab74549afd6443b97d2133
Louis <ls.duchemin@gmail.com>
2017-12-17 18:15:54 +0100

    Added directory upload widget Todo : clean and factorize

--- ipyfileupload/widgets.py
+++ ipyfileupload/widgets.py
@@ -80,3 +80,50 @@ class MultiFileUploadWidget(widgets.DOMWidget):
     def decode(byteStream):
         return io.StringIO(byteStream.decode('utf-8'))
         
+@widgets.register
+class DirectoryUploadWidget(widgets.DOMWidget):
+    '''File Upload Widget.
+    This widget provides file upload using `FileReader`.
+    '''
+    _view_name = Unicode('DirectoryUploadView').tag(sync=True)
+    _model_name = Unicode('DirectoryUploadModel').tag(sync=True)
+
+    _view_module = Unicode('ipyfileupload').tag(sync=True)
+    _model_module = Unicode('ipyfileupload').tag(sync=True)
+
+    _view_module_version = Unicode('^0.1.0').tag(sync=True)
+    _model_module_version = Unicode('^0.1.0').tag(sync=True)
+
+    label = Unicode(help='Label on button.').tag(sync=True)
+
+    base64_files = Dict(value_trait=Unicode(), help='Uploaded files').tag(sync=True)
+    files = Dict(value_trait=Bytes()).tag(sync=True)
+
+    def __init__(self, label="Browse", *args, **kwargs):
+        super(DirectoryUploadWidget, self).__init__(*args, **kwargs)
+        self._dom_classes += ('widget_item', 'btn-group')
+        self.label = label
+
+    @observe('base64_files')
+    def _base64_files_changed(self, *args):
+        for name, file in self.base64_files.items():
+            self.files[name] = base64.b64decode(file.split(',',1)[1])
+
+    def pop(self, file = None):
+        if file is None:
+            file, data_base64 = self.base64_files.popitem();
+            data = self.files.pop(file);
+        else:
+            data_base64 = self.base64_files.pop(file)
+            data = self.files.pop(file)
+
+        return {
+            'base64': data_base64,
+            'data': data,
+            'name': file
+        }
+
+    @staticmethod
+    def decode(byteStream):
+        return io.StringIO(byteStream.decode('utf-8'))
+        
--- js/lib/widgets.js
+++ js/lib/widgets.js
@@ -50,6 +50,27 @@ var _getId = (function() {
     }
 })();
 
+var DirectoryUploadModel = widgets.DOMWidgetModel.extend({
+    defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
+        _model_name: 'DirectoryUploadModel',
+        _view_name: 'DirectoryUploadView',
+        _model_module: 'ipyfileupload',
+        _view_module: 'ipyfileupload',
+        _model_module_version: '0.1.0',
+        _view_module_version: '0.1.0',
+    })
+});
+
+var _getId = (function() {
+
+    var cnt = 0;
+    return function() {
+
+        cnt += 1;
+        return 'fileupload_' + cnt;
+    }
+})();
+
 // Custom View. Renders the widget model.
 var FileUploadView = widgets.DOMWidgetView.extend({
     render: function render() {
@@ -185,11 +206,98 @@ var MultiFileUploadView = widgets.DOMWidgetView.extend({
 });
 
 
+// Custom View. Renders the widget model.
+var DirectoryUploadView = widgets.DOMWidgetView.extend({
+    render: function render() {
+        MultiFileUploadView.__super__.render.apply(this, arguments);
+        var id = _getId();
+        var label = this.model.get('label');
+        this.model.on('change:label', this._handleLabelChange, this);
+        var $label = $('<label />')
+            .text(label)
+            .addClass('btn btn-default')
+            .attr('for', id)
+            .appendTo(this.$el);
+
+        $('<input />')
+            .attr('type', 'file')
+            .attr('id', id)
+            .attr('mozdirectory', 'mozdirectory')
+            .attr('webkitdirectory', 'webkitdirectory')
+            .css('display', 'none')
+            .appendTo($label);
+    },
+
+    _handleLabelChange: function() {
+        var label = this.model.get('label');
+        this.$el.children("label").contents().first().replaceWith(label);
+    },
+
+    events: {
+        'change': '_handleFileChange'
+    },
+
+    readFile: function readFile(file, uploading) {
+        return new Promise(function(resolve, reject) {
+            reader = new FileReader();
+            console.log(file);
+            reader.onload = function fileReaderOnload(e) {
+                console.log('callback');
+                uploading[file.name] = e.target.result;
+                //that.model.set('data_base64', fileReader.result);
+                //that.touch();
+                console.log(uploading);
+                resolve(uploading);
+            }
+            reader.onerror = function fileReadOnError(e) {
+                reject(e);
+            }
+            reader.readAsDataURL(file);
+        });
+    },
+
+    _handleFileChange: function _handleFileChange(ev) {
+        var files = ev.target.files;
+        console.log(files);
+        var that = this;
+        var uploading = {};
+        var readers = [];
+        var promises = [];
+        (function(files) {
+            for (var i = 0; i < files.length; i++) {
+                promises.push(that.readFile(files[i], uploading));
+                /*
+                (function(file) {
+                    readers.push(new FileReader());
+                    console.log(i);
+                    console.log(file + "dsfgsdfg0");
+                    readers[i].onload = function fileReaderOnload(e) {
+                        console.log('callback', i);
+                        uploading[file.name] = e.target.result;
+                        //that.model.set('data_base64', fileReader.result);
+                        //that.touch();
+                        console.log(uploading);
+                    }
+                    readers[i].readAsArrayBuffer(file);
+                })(files[i]);
+                */
+            }
+        })(files);
+
+        Promise.all(promises).then(function() {
+            console.log(uploading);
+            that.model.set('base64_files', uploading);
+            that.touch();
+        });
+    }
+});
 
 
 module.exports = {
     FileUploadModel: FileUploadModel,
     FileUploadView: FileUploadView,
     MultiFileUploadModel: MultiFileUploadModel,
-    MultiFileUploadView: MultiFileUploadView
+    MultiFileUploadView: MultiFileUploadView,
+    DirectoryUploadModel: DirectoryUploadModel,
+    DirectoryUploadView: DirectoryUploadView
 };
