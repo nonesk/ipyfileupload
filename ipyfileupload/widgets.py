6883db8ad3806812fcdcdcecff5e5f48b70befac
Louis Duchemin <ls.duchemin@gmail.com>
2017-12-17 17:52:55 +0100

    MultiFile upload

--- ipyfileupload/widgets.py
+++ ipyfileupload/widgets.py
@@ -1,8 +1,8 @@
 import base64
+import io
 
 import ipywidgets as widgets
-from traitlets import Unicode, Bytes
-
+from traitlets import Unicode, Bytes, CBytes, Dict, observe
 
 
 @widgets.register
@@ -32,3 +32,51 @@ class FileUploadWidget(widgets.DOMWidget):
 
     def _data_base64_changed(self, *args):
         self.data = base64.b64decode(self.data_base64.split(',', 1)[1])
+
+@widgets.register
+class MultiFileUploadWidget(widgets.DOMWidget):
+    '''File Upload Widget.
+    This widget provides file upload using `FileReader`.
+    '''
+    _view_name = Unicode('MultiFileUploadView').tag(sync=True)
+    _model_name = Unicode('MultiFileUploadModel').tag(sync=True)
+
+    _view_module = Unicode('ipyfileupload').tag(sync=True)
+    _model_module = Unicode('ipyfileupload').tag(sync=True)
+
+    _view_module_version = Unicode('^0.1.0').tag(sync=True)
+    _model_module_version = Unicode('^0.1.0').tag(sync=True)
+
+    label = Unicode(help='Label on button.').tag(sync=True)
+
+    base64_files = Dict(value_trait=Unicode(), help='Uploaded files').tag(sync=True)
+    files = Dict(value_trait=Bytes()).tag(sync=True)
+
+    def __init__(self, label="Browse", *args, **kwargs):
+        super(MultiFileUploadWidget, self).__init__(*args, **kwargs)
+        self._dom_classes += ('widget_item', 'btn-group')
+        self.label = label
+
+    @observe('base64_files')
+    def _base64_files_changed(self, *args):
+        for name, file in self.base64_files.items():
+            self.files[name] = base64.b64decode(file.split(',',1)[1])
+
+    def pop(self, file = None):
+        if file is None:
+            file, data_base64 = self.base64_files.popitem();
+            data = self.files.pop(file);
+        else:
+            data_base64 = self.base64_files.pop(file)
+            data = self.files.pop(file)
+
+        return {
+            'base64': data_base64,
+            'data': data,
+            'name': file
+        }
+
+    @staticmethod
+    def decode(byteStream):
+        return io.StringIO(byteStream.decode('utf-8'))
+        
--- js/lib/widgets.js
+++ js/lib/widgets.js
@@ -20,19 +20,30 @@ require('jquery');
 // differ from the defaults will be specified.
 var FileUploadModel = widgets.DOMWidgetModel.extend({
     defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
-        _model_name : 'FileUploadModel',
-        _view_name : 'FileUploadView',
-        _model_module : 'ipyfileupload',
-        _view_module : 'ipyfileupload',
-        _model_module_version : '0.1.0',
-        _view_module_version : '0.1.0',
+        _model_name: 'FileUploadModel',
+        _view_name: 'FileUploadView',
+        _model_module: 'ipyfileupload',
+        _view_module: 'ipyfileupload',
+        _model_module_version: '0.1.0',
+        _view_module_version: '0.1.0',
     })
 });
 
-var _getId = (function () {
-    
+var MultiFileUploadModel = widgets.DOMWidgetModel.extend({
+    defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
+        _model_name: 'MultiFileUploadModel',
+        _view_name: 'MultiFileUploadView',
+        _model_module: 'ipyfileupload',
+        _view_module: 'ipyfileupload',
+        _model_module_version: '0.1.0',
+        _view_module_version: '0.1.0',
+    })
+});
+
+var _getId = (function() {
+
     var cnt = 0;
-    return function () {
+    return function() {
 
         cnt += 1;
         return 'fileupload_' + cnt;
@@ -41,22 +52,22 @@ var _getId = (function () {
 
 // Custom View. Renders the widget model.
 var FileUploadView = widgets.DOMWidgetView.extend({
-    render: function render () {
+    render: function render() {
         FileUploadView.__super__.render.apply(this, arguments);
         var id = _getId();
         var label = this.model.get('label');
         this.model.on('change:label', this._handleLabelChange, this);
         var $label = $('<label />')
-        .text(label)
-        .addClass('btn btn-default')
-        .attr('for', id)
-        .appendTo(this.$el);
+            .text(label)
+            .addClass('btn btn-default')
+            .attr('for', id)
+            .appendTo(this.$el);
 
         $('<input />')
-        .attr('type', 'file')
-        .attr('id', id)
-        .css('display', 'none')
-        .appendTo($label);
+            .attr('type', 'file')
+            .attr('id', id)
+            .css('display', 'none')
+            .appendTo($label);
     },
 
     _handleLabelChange: function() {
@@ -68,20 +79,19 @@ var FileUploadView = widgets.DOMWidgetView.extend({
         'change': '_handleFileChange'
     },
 
-    _handleFileChange: function _handleFileChange (ev) {
+    _handleFileChange: function _handleFileChange(ev) {
 
         var file = ev.target.files[0];
         var that = this;
         if (file) {
             var fileReader = new FileReader();
-            fileReader.onload = function fileReaderOnload () {
+            fileReader.onload = function fileReaderOnload() {
 
                 that.model.set('data_base64', fileReader.result);
                 that.touch();
             };
             fileReader.readAsDataURL(file);
-        }
-        else {
+        } else {
             that.send({ event: 'Unable to open file.' });
         }
         that.model.set('filename', file.name);
@@ -89,8 +99,97 @@ var FileUploadView = widgets.DOMWidgetView.extend({
     }
 });
 
+// Custom View. Renders the widget model.
+var MultiFileUploadView = widgets.DOMWidgetView.extend({
+    render: function render() {
+        MultiFileUploadView.__super__.render.apply(this, arguments);
+        var id = _getId();
+        var label = this.model.get('label');
+        this.model.on('change:label', this._handleLabelChange, this);
+        var $label = $('<label />')
+            .text(label)
+            .addClass('btn btn-default')
+            .attr('for', id)
+            .appendTo(this.$el);
+
+        $('<input />')
+            .attr('type', 'file')
+            .attr('id', id)
+            .attr('multiple', 'multiple')
+            .css('display', 'none')
+            .appendTo($label);
+    },
+
+    _handleLabelChange: function() {
+        var label = this.model.get('label');
+        this.$el.children("label").contents().first().replaceWith(label);
+    },
+
+    events: {
+        'change': '_handleFileChange'
+    },
+
+    readFile: function readFile(file, uploading) {
+        return new Promise(function(resolve, reject) {
+            reader = new FileReader();
+            console.log(file);
+            reader.onload = function fileReaderOnload(e) {
+                console.log('callback');
+                uploading[file.name] = e.target.result;
+                //that.model.set('data_base64', fileReader.result);
+                //that.touch();
+                console.log(uploading);
+                resolve(uploading);
+            }
+            reader.onerror = function fileReadOnError(e) {
+                reject(e);
+            }
+            reader.readAsDataURL(file);
+        });
+    },
+
+    _handleFileChange: function _handleFileChange(ev) {
+        var files = ev.target.files;
+        console.log(files);
+        var that = this;
+        var uploading = {};
+        var readers = [];
+        var promises = [];
+        (function(files) {
+            for (var i = 0; i < files.length; i++) {
+                promises.push(that.readFile(files[i], uploading));
+                /*
+                (function(file) {
+                    readers.push(new FileReader());
+                    console.log(i);
+                    console.log(file + "dsfgsdfg0");
+                    readers[i].onload = function fileReaderOnload(e) {
+                        console.log('callback', i);
+                        uploading[file.name] = e.target.result;
+                        //that.model.set('data_base64', fileReader.result);
+                        //that.touch();
+                        console.log(uploading);
+                    }
+                    readers[i].readAsArrayBuffer(file);
+                })(files[i]);
+                */
+            }
+        })(files);
+
+        Promise.all(promises).then(function() {
+            console.log(uploading);
+            that.model.set('base64_files', uploading);
+            that.touch();
+        });
+    }
+});
+
+
+
 
 module.exports = {
-    FileUploadModel : FileUploadModel,
-    FileUploadView : FileUploadView
+    FileUploadModel: FileUploadModel,
+    FileUploadView: FileUploadView,
+    MultiFileUploadModel: MultiFileUploadModel,
+    MultiFileUploadView: MultiFileUploadView
 };
